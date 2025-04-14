ebd56bb9f788e62d98026e6dd044c13ca79391f0
Mahendran Lavan <mahendranlavan29@gmail.com>
2025-04-14 23:18:00 +0530

    create-home-page

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+declare var $: any; // jQuery declaration
 @Component({
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      $('[data-toggle="owl"]').each(function (this: HTMLElement) {
+        const $this = $(this);
+        const options = $this.attr('data-owl-options') ? JSON.parse($this.attr('data-owl-options')!) : {};
+        if (!$this.hasClass('owl-loaded')) {
+          $this.owlCarousel(options);
+        }
+      });
+    }, 500); // <- try increasing delay to 500ms
+  }
 
 }
